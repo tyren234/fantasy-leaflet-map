589445ad458d848a526c1c8f59e7ff305ecc1389
Tymon <tyren234@gmail.com>
2024-05-24 13:53:10 +0200

    Now 'onzoomed' only removes tiles, and not all layers

--- js/app.js
+++ js/app.js
@@ -11,40 +11,41 @@ map.fitBounds(bounds);
 
 map.on("zoomend", function () {
     console.log(`Zoomed current zoom level: ${map.getZoom()}`);
-
-    map.eachLayer(function (layer) {
-        map.removeLayer(layer);
-    });
-
+    let images = [];
+    for (let i = 0; i < images.length; i++) {
+        images[i].removeFrom(map);
+    }
     // x is vertical
     // y is horizontal
     // 0,0 is in the bottom left corner
 
     if (map.getZoom() === 0) {
-        L.imageOverlay('img/maps/botw/tiles/0/0_0.jpg', bounds).addTo(map);
+        var image = L.imageOverlay('img/maps/botw/tiles/0/0_0.jpg', bounds).addTo(map);
+        images.push(image);
     } else if (map.getZoom() === 1) {
-        addTiles(2, map.getZoom());
+        addTiles(2, map.getZoom(), images);
         // L.imageOverlay('img/maps/botw/tiles/1/0_0.jpg', [[500, 0], [1000, 500]]).addTo(map);
         // L.imageOverlay('img/maps/botw/tiles/1/0_1.jpg', [[0, 0], [500, 500]]).addTo(map);
         // L.imageOverlay('img/maps/botw/tiles/1/1_0.jpg', [[500, 500], [1000, 1000]]).addTo(map);
         // L.imageOverlay('img/maps/botw/tiles/1/1_1.jpg', [[0, 500], [500, 1000]]).addTo(map);
     } else if (map.getZoom() === 2) {
-        addTiles(4, map.getZoom());
+        addTiles(4, map.getZoom(), images);
     } else if (map.getZoom() === 3) {
-        addTiles(8, map.getZoom());
+        addTiles(8, map.getZoom(), images);
     } else {
         L.imageOverlay('img/maps/botw/tiles/0/0_0.jpg', bounds).addTo(map);
     }
 })
 
 
-function addTiles(numberOfTilesAcross, zoomLevel) {
+function addTiles(numberOfTilesAcross, zoomLevel, containerList) {
     let boundStep = maxBound / numberOfTilesAcross;
 
     for (let column = 0; column < numberOfTilesAcross; column++) {
         for (let row = 0; row < numberOfTilesAcross; row++) {
             console.log(`Adding image (${zoomLevel}/${column}_${row}.jpg) with following bounds: ${[[maxBound - boundStep * (row + 1), column * boundStep], [maxBound - row * boundStep, boundStep * (column + 1)]]}`);
-            L.imageOverlay(`img/maps/botw/tiles/${zoomLevel}/${column}_${row}.jpg`, [[maxBound - boundStep * (row + 1), column * boundStep], [maxBound - row * boundStep, boundStep * (column + 1)]]).addTo(map);
+            var image = L.imageOverlay(`img/maps/botw/tiles/${zoomLevel}/${column}_${row}.jpg`, [[maxBound - boundStep * (row + 1), column * boundStep], [maxBound - row * boundStep, boundStep * (column + 1)]]).addTo(map);
+            containerList.push(image);
         }
     }
 }
